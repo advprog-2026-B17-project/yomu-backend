4b9e7a7d2f07c7e305cbe8b4868576a2a9b16a70
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-02-19 14:03:26 +0700

    springboot init

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/YomuBackendApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/yomubackend/YomuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
