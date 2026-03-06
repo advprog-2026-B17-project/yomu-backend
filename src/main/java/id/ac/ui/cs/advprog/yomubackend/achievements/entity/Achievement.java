57cf22ab2d1f65bbc0cedd85b0d7963838809269
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-03-06 20:05:32 +0700

    feat: add Achievement entity

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/achievements/entity/Achievement.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.yomubackend.achievements.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "achievements")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Achievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(nullable = false)
+    private String milestone;
+}
