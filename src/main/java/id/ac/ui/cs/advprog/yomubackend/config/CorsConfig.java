4107e26c56475db214647d10187a94b13f56b6d5
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-03-06 18:18:25 +0700

    fix typo url

--- src/main/java/id/ac/ui/cs/advprog/yomubackend/config/CorsConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://yomu-frontend-alpha.vercel.app/")
+                .allowedOrigins("http://localhost:3000", "https://yomu-frontend-alpha.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
