b2adc034dc05d04a8cb92348a3f1cf9bcbf732e0
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-04-17 19:22:18 +0700

    feat: add mappers

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/achievements/mapper/AchievementMapper.java
@@ -0,0 +1,38 @@
+package id.ac.ui.cs.advprog.yomubackend.achievements.mapper;
+
+import id.ac.ui.cs.advprog.yomubackend.achievements.dto.AchievementDto;
+import id.ac.ui.cs.advprog.yomubackend.achievements.dto.UserAchievementDto;
+import id.ac.ui.cs.advprog.yomubackend.achievements.entity.Achievement;
+import id.ac.ui.cs.advprog.yomubackend.achievements.entity.UserAchievement;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AchievementMapper {
+
+    public AchievementDto toDto(Achievement achievement) {
+        if (achievement == null)
+            return null;
+
+        return AchievementDto.builder()
+                .id(achievement.getId())
+                .name(achievement.getName())
+                .description(achievement.getDescription())
+                .conditionType(achievement.getConditionType())
+                .targetValue(achievement.getTargetValue())
+                .build();
+    }
+
+    public UserAchievementDto toUserAchievementDto(UserAchievement userAchievement) {
+        if (userAchievement == null)
+            return null;
+
+        return UserAchievementDto.builder()
+                .id(userAchievement.getId())
+                .userId(userAchievement.getUser().getId())
+                .achievement(toDto(userAchievement.getAchievement()))
+                .progress(userAchievement.getProgress())
+                .isCompleted(userAchievement.getIsCompleted())
+                .achievementDate(userAchievement.getAchievementDate())
+                .build();
+    }
+}
