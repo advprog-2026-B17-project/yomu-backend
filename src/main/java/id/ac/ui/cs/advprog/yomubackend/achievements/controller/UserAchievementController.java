17e763f773dd37d43e97f9d7eb02fc8f8d5cb2fa
valhize <190005533+Kritzeey@users.noreply.github.com>
2026-04-17 19:35:41 +0700

    feat: add UserAchievementController

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/achievements/controller/UserAchievementController.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.yomubackend.achievements.controller;
+
+import id.ac.ui.cs.advprog.yomubackend.achievements.dto.AchievementDto;
+import id.ac.ui.cs.advprog.yomubackend.achievements.dto.UserAchievementDto;
+import id.ac.ui.cs.advprog.yomubackend.achievements.service.AchievementService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class UserAchievementController {
+
+    private final AchievementService achievementService;
+
+    public UserAchievementController(AchievementService achievementService) {
+        this.achievementService = achievementService;
+    }
+
+    @GetMapping("/achievements")
+    public ResponseEntity<Page<AchievementDto>> getAllAchievements(
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(achievementService.getAllAchievements(pageable));
+    }
+
+    @GetMapping("/achievements/{id}")
+    public ResponseEntity<AchievementDto> getAchievementById(@PathVariable Long id) {
+        return ResponseEntity.ok(achievementService.getAchievementById(id));
+    }
+
+    @GetMapping("/users/{id}/achievements")
+    public ResponseEntity<Page<UserAchievementDto>> getUserAchievements(
+            @PathVariable Long id,
+            @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(achievementService.getUserAchievementProgress(id, pageable));
+    }
+}
