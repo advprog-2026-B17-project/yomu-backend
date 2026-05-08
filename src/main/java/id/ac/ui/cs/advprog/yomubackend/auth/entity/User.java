a9df53b5f3b1068fe7e57d4aeea099fc8a996ce9
Gerrybp12 <gerry.bimaputra@gmail.com>
2026-05-08 16:00:22 +0700

    feat: add googleId field to User entity

--- src/main/java/id/ac/ui/cs/advprog/yomubackend/auth/entity/User.java
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/auth/entity/User.java
@@ -35,6 +35,9 @@ public class User {
     @Column(name = "phone_number", unique = true)
     private String phoneNumber;
 
+    @Column(name = "google_id", unique = true)
+    private String googleId;
+
     @Column(name = "password_hash", nullable = false)
     private String password;
 
